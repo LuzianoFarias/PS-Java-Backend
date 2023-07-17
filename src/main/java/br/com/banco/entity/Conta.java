151754197567f0978ceda82cab1046db8450925f
LuzianoFarias <luziano-farias1993@hotmail.com>
2023-07-16 23:17:37 -0300

    Criando Entity

---
+++ src/main/java/br/com/banco/entity/Conta.java
@@ -0,0 +1,22 @@
+package br.com.banco.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idConta;
+    private String nomeResponsavel;
+
+}
---
+++ src/main/java/br/com/banco/entity/Transferencia.java
@@ -0,0 +1,37 @@
+package br.com.banco.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import br.com.banco.enums.TipoTransferencia;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Transferencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime dataTransferencia;
+    private Double valor;
+    @Enumerated(EnumType.STRING)
+    private TipoTransferencia tipo;
+    private String nomeOperadorTransacao;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id")
+    private Conta conta;
+
+}
